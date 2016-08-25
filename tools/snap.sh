22bd426b5ebd14677bec29c96c6ede6d06c60bf5
Jason Hobbs <jason.hobbs@gmail.com>
2016-08-25 17:34:51 -0500

    Fix envsubst to not substitue out filesets
    Change-Id: I0154e991cd410b764d5c77879453de607bdadadb

--- tools/snap.sh
+++ tools/snap.sh
@@ -3,5 +3,5 @@ set -ue
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
 export TEMPEST_VERSION=$(git describe --dirty)
-envsubst < snapcraft.yaml.tmpl > snapcraft.yaml
+envsubst '$TEMPEST_VERSION' < snapcraft.yaml.tmpl > snapcraft.yaml
 snapcraft snap
